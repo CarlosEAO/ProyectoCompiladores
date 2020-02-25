8fc4a7518ffef5904771f6c2e60bb48480aba89f
Carlos Aranda <carlos.eao15@gmail.com>
2020-02-25 12:46:37 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoCompiladores/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace ProyectoCompiladores
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.codeRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.saveFileAsButton = new System.Windows.Forms.Button();
+            this.saveFileButton = new System.Windows.Forms.Button();
+            this.fileNameLabel = new System.Windows.Forms.Label();
+            this.compileButton = new System.Windows.Forms.Button();
+            this.newFileButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // codeRichTextBox
+            // 
+            this.codeRichTextBox.AcceptsTab = true;
+            this.codeRichTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.codeRichTextBox.ForeColor = System.Drawing.Color.White;
+            this.codeRichTextBox.Location = new System.Drawing.Point(115, 95);
+            this.codeRichTextBox.Name = "codeRichTextBox";
+            this.codeRichTextBox.Size = new System.Drawing.Size(575, 303);
+            this.codeRichTextBox.TabIndex = 0;
+            this.codeRichTextBox.Text = "";
+            this.codeRichTextBox.TextChanged += new System.EventHandler(this.codeRichTextBox_TextChanged);
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Location = new System.Drawing.Point(312, 404);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(75, 23);
+            this.openFileButton.TabIndex = 1;
+            this.openFileButton.Text = "Abrir";
+            this.openFileButton.UseVisualStyleBackColor = true;
+            this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
+            // 
+            // saveFileAsButton
+            // 
+            this.saveFileAsButton.Location = new System.Drawing.Point(488, 404);
+            this.saveFileAsButton.Name = "saveFileAsButton";
+            this.saveFileAsButton.Size = new System.Drawing.Size(111, 23);
+            this.saveFileAsButton.TabIndex = 2;
+            this.saveFileAsButton.Text = "Guardar como";
+            this.saveFileAsButton.UseVisualStyleBackColor = true;
+            this.saveFileAsButton.Click += new System.EventHandler(this.saveFileAsButton_Click);
+            // 
+            // saveFileButton
+            // 
+            this.saveFileButton.Location = new System.Drawing.Point(393, 404);
+            this.saveFileButton.Name = "saveFileButton";
+            this.saveFileButton.Size = new System.Drawing.Size(75, 23);
+            this.saveFileButton.TabIndex = 3;
+            this.saveFileButton.Text = "Guardar";
+            this.saveFileButton.UseVisualStyleBackColor = true;
+            this.saveFileButton.Click += new System.EventHandler(this.saveFileButton_Click);
+            // 
+            // fileNameLabel
+            // 
+            this.fileNameLabel.AutoSize = true;
+            this.fileNameLabel.Location = new System.Drawing.Point(124, 35);
+            this.fileNameLabel.Name = "fileNameLabel";
+            this.fileNameLabel.Size = new System.Drawing.Size(0, 17);
+            this.fileNameLabel.TabIndex = 4;
+            // 
+            // compileButton
+            // 
+            this.compileButton.Location = new System.Drawing.Point(615, 404);
+            this.compileButton.Name = "compileButton";
+            this.compileButton.Size = new System.Drawing.Size(75, 23);
+            this.compileButton.TabIndex = 5;
+            this.compileButton.Text = "Compilar";
+            this.compileButton.UseVisualStyleBackColor = true;
+            this.compileButton.Click += new System.EventHandler(this.compileButton_Click);
+            // 
+            // newFileButton
+            // 
+            this.newFileButton.Location = new System.Drawing.Point(143, 405);
+            this.newFileButton.Name = "newFileButton";
+            this.newFileButton.Size = new System.Drawing.Size(120, 23);
+            this.newFileButton.TabIndex = 6;
+            this.newFileButton.Text = "Nuevo archivo";
+            this.newFileButton.UseVisualStyleBackColor = true;
+            this.newFileButton.Click += new System.EventHandler(this.newFileButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.newFileButton);
+            this.Controls.Add(this.compileButton);
+            this.Controls.Add(this.fileNameLabel);
+            this.Controls.Add(this.saveFileButton);
+            this.Controls.Add(this.saveFileAsButton);
+            this.Controls.Add(this.openFileButton);
+            this.Controls.Add(this.codeRichTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox codeRichTextBox;
+        private System.Windows.Forms.Button openFileButton;
+        private System.Windows.Forms.Button saveFileAsButton;
+        private System.Windows.Forms.Button saveFileButton;
+        private System.Windows.Forms.Label fileNameLabel;
+        private System.Windows.Forms.Button compileButton;
+        private System.Windows.Forms.Button newFileButton;
+    }
+}
+
---
+++ ProyectoCompiladores/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+namespace ProyectoCompiladores
+{
+    public partial class Form1 : Form
+    {
+        #region "Avoid flickering"
+
+        private const int WM_SETREDRAW = 0xB;
+
+        [System.Runtime.InteropServices.DllImport("User32")]
+
+        private static extern bool SendMessage(IntPtr hWnd, int msg, int wParam, int lParam);
+
+        private void FreezeDraw()
+        { //Disable drawing
+            SendMessage(codeRichTextBox.Handle, WM_SETREDRAW, 0, 0);
+        }
+        private void UnfreezeDraw()
+        { //Enable drawing and do a redraw.
+            SendMessage(codeRichTextBox.Handle, WM_SETREDRAW, 1, 0);
+            codeRichTextBox.Invalidate(true);
+        }
+        #endregion
+
+        OpenFileDialog ofd;
+        SaveFileDialog sfd;
+
+        String[] keywords = { "public", "void", "using", "static", "class" };
+        String[] dataTypes = { "int", "bool", "char", "string" };
+
+        HashSet<string> keywordsHashSet;
+        HashSet<string> dataTypesHashSet;
+
+        bool fileOpened;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            keywordsHashSet = new HashSet<string>(keywords);
+            dataTypesHashSet = new HashSet<string>(dataTypes);
+            codeRichTextBox.SelectionFont = new Font("Courier New", 10, FontStyle.Regular);
+            fileNameLabel.Text = "Nuevo archivo";
+            fileOpened = false;
+        }
+
+        private void openFileButton_Click(object sender, EventArgs e)
+        {
+            ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            string readFile = "";
+
+            string fileName = ofd.FileName;
+            if (fileName != "")
+            {
+                codeRichTextBox.Text = "";
+                readFile = File.ReadAllText(ofd.FileName);
+                Parse(readFile);
+                fileNameLabel.Text = ofd.FileName;
+                fileOpened = true;
+            }          
+        }
+
+        private void saveFileButton_Click(object sender, EventArgs e)
+        {
+            if (fileOpened)
+            {
+                codeRichTextBox.SaveFile(fileNameLabel.Text, RichTextBoxStreamType.PlainText);
+            }
+            else
+            {
+                sfd = new SaveFileDialog();
+                sfd.ShowDialog();
+                if (sfd.FileName != "")
+                {
+                    codeRichTextBox.SaveFile(sfd.FileName, RichTextBoxStreamType.PlainText);
+                    fileOpened = true;
+                    fileNameLabel.Text = sfd.FileName;
+                }
+            }
+        }
+
+
+        void Parse(string code)
+        {
+            // Foreach line in input,
+            // identify key words and format them when adding to the rich text box.
+            Regex r = new Regex("\\n");
+            string[] lines = r.Split(code);
+            foreach (string l in lines)
+            {
+                ParseLine(l);
+            }
+        }
+
+        void ParseLine(string line)
+        {
+            Regex r = new Regex("([ \\t{}();])");
+            String[] tokens = r.Split(line);
+
+            foreach (string token in tokens)
+            {
+                // Set the token's default color and font.
+                codeRichTextBox.SelectionColor = Color.White;
+
+                // Check for a comment.
+                if (token == "//" || token.StartsWith("//"))
+                {
+                    // Find the start of the comment and then extract the whole comment.
+                    int index = line.IndexOf("//");
+                    string comment = line.Substring(index, line.Length - index);
+                    codeRichTextBox.SelectionColor = Color.LightGreen;
+                    codeRichTextBox.SelectedText = comment;
+                    break;
+                }
+
+                // Check whether the token is a keyword. 
+
+                colorToken(token);
+
+                codeRichTextBox.SelectedText = token;
+            }
+            codeRichTextBox.SelectedText = "\n";
+        }
+
+        private void codeRichTextBox_TextChanged(object sender, EventArgs e)
+        {
+            FreezeDraw();
+            // Calculate the starting position of the current line.  
+            int start = 0, end = 0;
+            for (start = codeRichTextBox.SelectionStart-1; start >=0; start--)
+            {
+                if (codeRichTextBox.Text[start] == '\n') { start++; break; }
+            }
+            if (start < 0) start = 0;
+            // Calculate the end position of the current line.  
+            for (end = codeRichTextBox.SelectionStart; end < codeRichTextBox.Text.Length; end++)
+            {
+                if (codeRichTextBox.Text[end] == '\n') break;
+            }
+            // Extract the current line that is being edited.  
+            String line = codeRichTextBox.Text.Substring(start, end - start);
+            
+            // Backup the users current selection point.  
+            int selectionStart = codeRichTextBox.SelectionStart;
+            int selectionLength = codeRichTextBox.SelectionLength;
+
+            // Split the line into tokens.  
+            Regex r = new Regex("([ \\t{}();])");
+            string[] tokens = r.Split(line);
+            int index = start;
+            foreach (string token in tokens)
+            {
+                codeRichTextBox.SelectionStart = index;
+                codeRichTextBox.SelectionLength = token.Length;
+                // Set the token's default color and font.  
+                
+                codeRichTextBox.SelectionColor = Color.White;
+
+                // Check for a comment.
+                if (token == "//" || token.StartsWith("//"))
+                {
+                    // Find the start of the comment and then extract the whole comment.
+                    int length = line.Length - (index - start);
+                    string commentText = codeRichTextBox.Text.Substring(index, length);
+                    codeRichTextBox.SelectionStart = index;
+                    codeRichTextBox.SelectionLength = length;
+                    codeRichTextBox.SelectionColor = Color.LightGreen;
+                    break;
+                }
+                // Check whether the token is a keyword. 
+                colorToken(token);
+                
+                index+= token.Length;
+                
+            }
+            // Restore the users current selection point.   
+            codeRichTextBox.SelectionStart = selectionStart;
+            codeRichTextBox.SelectionLength = selectionLength;
+
+            UnfreezeDraw();
+        }
+
+        private void colorToken(string token){
+            if (keywordsHashSet.Contains(token))
+            {
+                codeRichTextBox.SelectionColor = Color.Blue;
+            }
+
+            if (dataTypesHashSet.Contains(token))
+            {
+                codeRichTextBox.SelectionColor = Color.MediumPurple;
+            }
+        }
+
+        private void compileButton_Click(object sender, EventArgs e)
+        {
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
+
+            cmd.StandardInput.WriteLine("cd C:/Users/Carlos/desktop");
+            cmd.StandardInput.WriteLine("mkdir carpeta");
+            cmd.StandardInput.WriteLine("echo 'hola'");
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            cmd.WaitForExit();
+            Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+
+        }
+
+        private void saveFileAsButton_Click(object sender, EventArgs e)
+        {
+            sfd = new SaveFileDialog();
+            sfd.ShowDialog();
+            if (sfd.FileName != "")
+            {
+                codeRichTextBox.SaveFile(sfd.FileName, RichTextBoxStreamType.PlainText);
+                fileOpened = true;
+                fileNameLabel.Text = sfd.FileName;
+            }
+        }
+
+        private void newFileButton_Click(object sender, EventArgs e)
+        {
+            codeRichTextBox.Text = "";
+            fileNameLabel.Text = "Nuevo archivo";
+        }
+    }
+}
